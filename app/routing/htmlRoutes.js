ea77d5f8f49a8290605c5c98b4c12e7392130cd1
Susan Chiemi <Susan@Users-MacBook.local>
2017-09-02 12:07:47 -0400

    commit

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -10,8 +10,8 @@ module.exports = function(app) {
         res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
 
-// If no matching route is found default to home
-// app.use(function(req, res) {
-//     res.sendFile(path.join(__dirname, "/../public/home.html"));
-//   });
+//If no matching route is found default to home
+app.use(function(req, res) {
+res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
 };
