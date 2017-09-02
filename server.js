15ebea49a9ddb2e893367fef8a0bf60836790aea
Susan Chiemi <Susan@Users-MacBook.local>
2017-09-02 11:51:25 -0400

    new commit

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 //added this 8/29/2017
-app.use(express.static(path.join(__dirname, 'app/public')));
+app.use(express.static(path.join(__dirname, "./app/public")));
 
 // BodyParser - standard
 app.use(bodyParser.json());
